0c321538998f5a87c908e30a3354eb7dd7ed5a8f
eldemet <eldemet@gmail.com>
2016-12-31 02:11:13 +0200

    update epanet-matlab-toolkit

---
+++ epanet-matlab-toolkit/RunTests.m
@@ -0,0 +1,15 @@
+% Run Tests Files
+
+addpath(genpath(pwd))
+
+% Based functions
+testFunctions
+
+% Msx Functions
+testMSXFunctions
+
+% Bin Functions
+testBinFunctions
+
+% Run epanet without DLL
+testBinWithoutDLL
